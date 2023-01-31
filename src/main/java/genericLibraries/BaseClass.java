1ffe82e17ef2659d38d6c20bbe2b2018fa5c1428
Admin <Admin@Kalyani>
2023-01-31 11:43:49 +0530

    First commit

---
+++ src/main/java/genericLibraries/BaseClass.java
@@ -0,0 +1,69 @@
+package genericLibraries;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+
+import pomPages.ContactUspage;
+import pomPages.CoreJavaDemoVideoPage;
+import pomPages.CoreJavaForSeleniumPage;
+import pomPages.SeleniumTrainingPage;
+import pomPages.SkillraryDemoAppPage;
+import pomPages.SkillraryHomePage;
+import pomPages.TestingPage;
+
+public class BaseClass {
+	protected PropertiesFileUtility property;
+	protected ExcelFileUtility excel;
+	protected WebDriverUtility web;
+	protected WebDriver driver;
+	protected SkillraryHomePage home;
+	protected SkillraryDemoAppPage demoApp;
+	protected ContactUspage contact;
+	protected SeleniumTrainingPage selenium;
+	protected TestingPage testing;
+	protected CoreJavaForSeleniumPage coreJava;
+	protected CoreJavaDemoVideoPage javaDemo;
+//@BeforeSuite
+//@BeforeTest
+@BeforeClass
+public void classSetup() {
+	property = new PropertiesFileUtility();
+	excel = new ExcelFileUtility();
+	web =new WebDriverUtility();
+	
+	property.propertiesIntializaation(IConstantPath.PROPERTY_FILEPATH);
+	String browser = property.fetchProperty("browser");
+	String url = property.fetchProperty("url");
+	long time = Long.parseLong(property.fetchProperty("timeouts"));
+	
+	driver = web.openApplication(browser, url, time);
+	home = new SkillraryHomePage(driver);
+	Assert.assertTrue(home.getLogo().isDisplayed());
+	
+}
+@BeforeMethod
+public void methodSetup() {
+	excel.excelInitialization(IConstantPath.EXCEL_FILE_PATH);
+	demoApp = new SkillraryDemoAppPage(driver);
+	contact = new ContactUspage(driver);
+	selenium = new SeleniumTrainingPage(driver);
+	testing = new TestingPage(driver);
+	coreJava = new CoreJavaForSeleniumPage(driver);
+	javaDemo = new CoreJavaDemoVideoPage(driver);
+	
+}
+@AfterMethod
+public void methodTearDown() {
+	excel.closeWorkbook();
+}
+@AfterClass
+public void classTearDown() {
+web.quitBrowser();	
+}
+//@AfterTest
+//@AfterSuite
+}
---
+++ src/main/java/genericLibraries/IConstantPath.java
@@ -0,0 +1,13 @@
+package genericLibraries;
+
+/**
+ * This interface holds the properties as well as excel file paths used throughout the framework
+ * 
+ * @author Kalyani
+ *
+ */
+public interface IConstantPath {
+	String PROPERTY_FILEPATH =".\\src\\test\\resources\\commondata.properties";
+	String EXCEL_FILE_PATH = ".\\src\\test\\resources\\ContactTestData.xlsx";
+
+}
